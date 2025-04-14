4052036b6752d71111298496b3277e05af723f1b
BernalFA <fabernal1230@gmail.com>
2025-04-14 10:14:46 +0200

    add documentation and type hinting

--- predfluor/utils.py
+++ predfluor/utils.py
@@ -1,12 +1,14 @@
 """
-Module for featurization and prediction of fluorescence based on notebooks in original
-repo.
+Module containing the FluorescencePredictor class for molecular featurization and
+prediction of fluorescence and quantum yield of small molecules (from smiles),
+based on the notebooks provided in the original repo.
 
 @author: Dr. Freddy Bernal
 """
 
 import warnings
 from collections import namedtuple
+from collections.abc import Iterable
 from pathlib import Path
 
 import joblib
@@ -20,14 +22,15 @@ from rdkit.Chem import (
     rdMolDescriptors,
 )
 
-
+# Define module path
 MOD_PATH = Path(__file__).parent
 
+# Instantiate namedtuples for models and scalers
 Models = namedtuple("Models", "wl qy")
 Scalers = namedtuple("Scalers", "wl qy")
 
 
-def flatten_list(lst):
+def flatten_list(lst: list) -> list:
     flat_list = []
     for item in lst:
         if isinstance(item, list):
@@ -38,6 +41,25 @@ def flatten_list(lst):
 
 
 class FluorescencePredictor:
+    """Class for molecular featurization and prediction of fluorescence wavelength and
+    quantum yield of (small) organic molecules (as SMILES strings).
+    It uses the models presented in Souza et al. (DOI: 10.1021/acs.jcim.4c02403)
+
+    Example:
+        smiles = ["Cc1c(cc(cc1[N+](=O)[O-])[N+](=O)[O-])[N+](=O)[O-]",
+                  "c2ccc(CNc1ccccc1)cc2"]
+        solvents = ["O", "C1CCCCC1"]
+
+        predictor = FluorescencePredictor()
+        results = predictor.predict(smiles, solvents)
+
+    results is a numpy array containing the wavelengths (results[:, 0]) and the quantum
+    yields (results[:, 1]) predicted by the trained models.
+
+    IMPORTANT: invalid SMILES are processed as a zero-containing feature vector. Thus,
+    they still produce some predicted values. Please check the given warnings in case
+    of invalid SMILES.
+    """
 
     def __init__(self):
         self.models = None
@@ -45,6 +67,7 @@ class FluorescencePredictor:
         self._initialize()
 
     def _initialize(self):
+        """Load trained models and scalars as attributes (named tuples)."""
         model_qy = joblib.load((MOD_PATH / "../Models/QY_Random.pkl").resolve())
         model_wl = joblib.load((MOD_PATH / "../Models/WL_Random.pkl").resolve())
         scaler_wl = joblib.load((MOD_PATH / "../Models/scaler_modelwl.pkl").resolve())
@@ -52,17 +75,42 @@ class FluorescencePredictor:
         self.models = Models(model_wl, model_qy)
         self.scalers = Scalers(scaler_wl, scaler_qy)
 
-    def _get_morgan_fp(self, m):
+    def _get_morgan_fp(self, m: Chem.Mol) -> np.ndarray:
+        """Create Morgan fingerprint for radius 2 (512 bits) for given molecule.
+
+        Args:
+            m (Chem.Mol): RDKit mol object.
+
+        Returns:
+            np.ndarray: generated fingerprint (len = 512).
+        """
         fp = AllChem.GetMorganFingerprintAsBitVect(m, 2, 512)
         fp = np.array(list(fp), dtype=np.int16)
         return fp
 
-    def _get_maccs_keys(self, m):
+    def _get_maccs_keys(self, m: Chem.Mol) -> np.ndarray:
+        """Create MACCS fingerprint for given molecule.
+
+        Args:
+            m (Chem.Mol): RDKit mol object.
+
+        Returns:
+            np.ndarray: generated fingerprint.
+        """
         fp = rdMolDescriptors.GetMACCSKeysFingerprint(m)
         fp = np.array(list(fp), dtype=np.int16)
         return fp
 
-    def _get_rdkit_descriptors(self, m):
+    def _get_rdkit_descriptors(self, m: Chem.Mol) -> np.ndarray:
+        """Calculate RDKit selected descriptors for given molecule. Some descriptor
+        are scaled by default.
+
+        Args:
+            m (Chem.Mol): RDKit mol object.
+
+        Returns:
+            np.ndarray: calculated descriptors (len = 39).
+        """
         desc = [
             1000 * Descriptors.FpDensityMorgan1(m),
             1000 * Descriptors.FpDensityMorgan2(m),
@@ -107,7 +155,16 @@ class FluorescencePredictor:
         desc = np.array(desc, dtype=np.int16)
         return desc
 
-    def _get_lipinski_desc(self, m):
+    def _get_lipinski_desc(self, m: Chem.Mol) -> np.ndarray:
+        """Calculate Lipinski's rule of five related and graph-based descriptors for
+        given molecule.
+
+        Args:
+            m (Chem.Mol): RDKit mol object.
+
+        Returns:
+            np.ndarray: calculated descriptors (len = 7).
+        """
         desc = [
             Lipinski.HeavyAtomCount(m),
             Lipinski.NHOHCount(m),
@@ -120,15 +177,34 @@ class FluorescencePredictor:
         desc = np.array(desc, dtype=np.int16)
         return desc
 
-    def _get_bcut2d_desc(self, m):
+    def _get_bcut2d_desc(self, m: Chem.Mol) -> np.ndarray:
+        """Calculate BTCU descriptors for given molecule (as implemented in RDKit).
+
+        Args:
+            m (Chem.Mol): RDKit mol object.
+
+        Returns:
+            np.ndarray: calculated descriptors.
+        """
         desc = [rdMolDescriptors.BCUT2D(m)]
         desc = np.array(flatten_list(desc), dtype=np.int16)
         return desc
 
-    def get_features(self, smiles):
+    def get_features(self, smiles: Iterable[str]) -> np.ndarray:
+        """Calculate all the necessary molecular features for prediction of fluorescence
+        wavelength and quantum yield for given .
+
+        Args:
+            smiles (Iterable[str]): SMILES strings for molecules to be predicted.
+
+        Returns:
+            np.ndarray: concatenated set of molecular features (len = 733).
+        """
+        # Iterate over molecules
         features_list = []
         for smi in smiles:
             m = Chem.MolFromSmiles(smi)
+            # Calculate features for valid SMILES
             if m is not None:
                 feat1 = self._get_morgan_fp(m)
                 feat2 = self._get_maccs_keys(m)
@@ -137,28 +213,75 @@ class FluorescencePredictor:
                 feat5 = self._get_bcut2d_desc(m)
                 features = np.concatenate([feat1, feat2, feat3, feat4, feat5])
             else:
-                warnings.warn(f"WARNING: {smi} could not be converted to RDKit Mol")
+                # Display warning for invalid SMILES and add a zeros containing array
+                # instead
+                warnings.warn(f"\nWARNING: {smi} could not be converted to RDKit Mol\n")
                 features = np.zeros((733))  # 733 is the number of features
             features_list.append(features)
         return np.array(features_list)
 
-    def _calculate_features(self, smiles, solv_smiles):
+    def _calculate_features(
+        self, smiles: Iterable[str], solv_smiles: Iterable[str]
+    ) -> np.ndarray:
+        """calculate a combined molecular features vector for the molecule and the
+        corresponding solvent. It uses get_features.
+
+        Args:
+            smiles (Iterable[str]): SMILES string for molecules of interest.
+            solv_smiles (Iterable[str]): SMILES string for solvents (the number of
+                                         solvents must be equal to the number of
+                                         compounds).
+
+        Returns:
+            np.ndarray: concatenated features.
+        """
         features_cmpd = self.get_features(smiles)
         features_solv = self.get_features(solv_smiles)
         features = np.hstack([features_cmpd, features_solv])
         return features
 
-    def _predict_wavelength(self, features):
+    def _predict_wavelength(self, features: np.ndarray) -> np.ndarray:
+        """Calculate fluorescence wavelength from given set of molecular features.
+
+        Args:
+            features (np.ndarray): combined molecular features for compound and solvent
+                                   obtained with _calculate_features.
+
+        Returns:
+            np.ndarray: predicted wavelength of maximum fluorescence.
+        """
         features_scaled_wl = self.scalers.wl.transform(features)
         predicted_wl = self.models.wl.predict(features_scaled_wl)
         return predicted_wl
 
-    def _predict_quantum_yield(self, features):
+    def _predict_quantum_yield(self, features: np.ndarray) -> np.ndarray:
+        """Calculate quantum yield from given set of molecular features.
+
+        Args:
+            features (np.ndarray): combined molecular features for compound and solvent
+                                   obtained with _calculate_features.
+
+        Returns:
+            np.ndarray: predicted quantum yield of maximum fluorescence.
+        """
         features_scaled_qy = self.scalers.qy.transform(features)
         predicted_qy = self.models.qy.predict(features_scaled_qy)
         return predicted_qy
 
-    def predict(self, smiles, solv_smiles="O"):
+    def predict(self, smiles: Iterable[str], solv_smiles: Iterable[str]) -> np.ndarray:
+        """Use trained models to predict the wavelength and quantum yield of maximum
+        fluorescence of (small) organic molecules for given compounds in defined
+        solvents (as SMILES strings).
+
+        Args:
+            smiles (Iterable[str]): SMILES strings for query compounds.
+            solv_smiles (Iterable[str]): SMILES strings for solvents disolving the
+                                         compounds.
+
+        Returns:
+            np.ndarray: predicted wavelength and quantum yield (first and second column,
+                        respectively).
+        """
         features = self._calculate_features(smiles, solv_smiles)
         predicted_wl = self._predict_wavelength(features)
         predicted_qy = self._predict_quantum_yield(features)
